72e73ba2f8fdc0eda53b593f9f1de913d7909944
David V. Lu <davidvlu@gmail.com>
2026-02-13 00:25:13 -0500

    Random checks

--- color_util/test/utest.py
+++ color_util/test/utest.py
@@ -1,5 +1,8 @@
-from color_util import ColorHSVA24, ColorRGBA24
+from color_util import ColorHSVA, ColorHSVA24, ColorRGBA, ColorRGBA24
 from color_util import convert_color_to_float, convert_color_to_int
+from color_util import convert_color_to_rgb, convert_color_to_hsv
+
+import pytest
 
 
 def test_full_color():
@@ -17,3 +20,50 @@ def test_full_color():
 
                     assert rgba24 == convert_color_to_int(rgba)
                     assert hsva24 == convert_color_to_int(hsva)
+
+
+def check_conversion(r, g, b, h, s, v, a=0.9, epsilon=0.001):
+    rgba = ColorRGBA(r, g, b, a)
+    hsva = ColorHSVA(h, s, v, a)
+
+    converted_rgba = convert_color_to_rgb(hsva)
+
+    assert converted_rgba.r == pytest.approx(r, abs=epsilon)
+    assert converted_rgba.g == pytest.approx(g, abs=epsilon)
+    assert converted_rgba.b == pytest.approx(b, abs=epsilon)
+    assert converted_rgba.a == pytest.approx(a, abs=epsilon)
+
+    converted_hsva = convert_color_to_hsv(rgba)
+    assert converted_hsva.h == pytest.approx(h, abs=epsilon)
+    assert converted_hsva.s == pytest.approx(s, abs=epsilon)
+    assert converted_hsva.v == pytest.approx(v, abs=epsilon)
+    assert converted_hsva.a == pytest.approx(a, abs=epsilon)
+
+
+def test_random_color_check():
+    # black
+    check_conversion(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+    # white
+    check_conversion(1.0, 1.0, 1.0, 0.0, 0.0, 1.0)
+
+    # red
+    check_conversion(1.0, 0.0, 0.0, 0.0, 1.0, 1.0)
+
+    # green
+    check_conversion(0.0, 1.0, 0.0, 0.33333, 1.0, 1.0)
+
+    # blue
+    check_conversion(0.0, 0.0, 1.0, 0.66667, 1.0, 1.0)
+
+    # cyan
+    check_conversion(0.0, 1.0, 1.0, 0.5, 1.0, 1.0)
+
+    # magenta
+    check_conversion(1.0, 0.0, 1.0, 0.83333, 1.0, 1.0)
+
+    # yellow
+    check_conversion(1.0, 1.0, 0.0, 0.16667, 1.0, 1.0)
+
+    # gray
+    check_conversion(0.8, 0.8, 0.8, 0.0, 0.0, 0.8)
