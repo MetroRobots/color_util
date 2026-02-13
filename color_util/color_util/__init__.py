9dee01ca4fab67ea03004b9c746f55c8745c40bf
David V. Lu <davidvlu@gmail.com>
2026-02-12 23:13:55 -0500

    Initial Python

---
+++ color_util/color_util/__init__.py
@@ -0,0 +1,4 @@
+from .types import ColorHSVA, ColorHSVA24, ColorRGBA, ColorRGBA24
+from .named_colors import NamedColor
+
+__all__ = ['ColorHSVA', 'ColorHSVA24', 'ColorRGBA', 'ColorRGBA24', 'NamedColor']
---
+++ color_util/color_util/types.py
@@ -0,0 +1,41 @@
+
+class GenericColorRGBA:
+    def __init__(self, r, g, b, a):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+
+    def __repr__(self):
+        return f'(r: {self.r}, g: {self.g}, b: {self.b}, a: {self.a})'
+
+
+class GenericColorHSVA:
+    def __init__(self, h, s, v, a):
+        self.h = h
+        self.s = s
+        self.v = v
+        self.a = a
+
+    def __repr__(self):
+        return f'(h: {self.h}, s: {self.s}, v: {self.v}, a: {self.a})'
+
+
+class ColorRGBA(GenericColorRGBA):
+    def __init__(self, r=0.0, g=0.0, b=0.0, a=1.0):
+        super().__init__(r, g, b, a)
+
+
+class ColorRGBA24(GenericColorRGBA):
+    def __init__(self, r=0, g=0, b=0, a=255):
+        super().__init__(r, g, b, a)
+
+
+class ColorHSVA(GenericColorHSVA):
+    def __init__(self, h=0.0, s=0.0, v=0.0, a=1.0):
+        super().__init__(h, s, v, a)
+
+
+class ColorHSVA24(GenericColorHSVA):
+    def __init__(self, h=0, s=0, v=0, a=255):
+        super().__init__(h, s, v, a)
